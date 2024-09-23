e1623a5263bb172549cca2eacce53e9c9bcce95e
Huzaifa Sajjad <140931462+HUZAIFASAJJAD012@users.noreply.github.com>
2024-09-23 04:11:59 -0700

    Usestatearray

--- react_hoots/src/App.jsx
+++ react_hoots/src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import Usestate from "./component/usestate";
 import Rule from './component/rule';
+import { Usestatearray } from './component/Usestatearray';
 
 function App() {
 
+  
 
   return (
     <> 
     <Usestate/>
      <Rule/>
+     <Usestatearray/>
     </>
   )
 }
---
+++ react_hoots/src/component/Usestatearray.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+
+export const Usestatearray = () => {
+    const [student,setstudent]=useState(
+        {Name:'huzaifa',age:24},
+        {Name:'ali',age:34},
+        {Name:'mubarek',age:54},
+        {Name:'hassan',age:24},
+    );
+
+  return (
+    <div>
+        
+student.map
+
+
+    </div>
+  )
+}
